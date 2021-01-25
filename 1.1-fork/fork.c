278c1962f1738f04324157c8de199ba035b5e4ed
Wheados <Wheadosz@hotmail.com>
2021-01-25 00:15:41 -0300

    created fork tree

--- 1.1-fork/fork.c
+++ 1.1-fork/fork.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+
+int anoPassados(const time_t start){
+    time_t clock = time(NULL);
+
+    return clock - start;
+}
 
 int main(){
-    fork();
-    printf("Hello world \n");
+    time_t start,stop;
+    start = time(NULL);
+    int year = 0;
 
+
+    sleep(14);
+    printf("Nascimento do Filho1. Ano: %ld \n", anoPassados(start));
+    int p = fork();
+    if (p > 0){ //Pai
+        int pp = fork(); //Criar Filho2
+
+        if(pp == 0){ //Filho2
+            int pf2 = fork(); //Criar Neto2
+
+            if (pf2 == 0){ //Neto2
+
+            }
+        }
+
+    }
+    
+    if (p == 0){ //Filho
+        int pf = fork();
+
+        if (pf == 0){ //Neto1
+
+        }
+
+    }
     return 0;
 }
