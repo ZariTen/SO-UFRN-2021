7ada1e77c46caabc56a5223a3db14fd010628e8e
Wheados <Wheadosz@hotmail.com>
2021-01-28 11:43:02 -0300

    sleep timings

--- 1.1-fork/fork.c
+++ 1.1-fork/fork.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <signal.h>
 
 int anoPassados(const time_t start){
     time_t clock = time(NULL);
@@ -17,14 +18,18 @@ int main(){
 
 
     sleep(14);
-    printf("%ld \n",anoPassados(start));
-    printf("Nascimento do Filho1. Hora: %ld:%ld:%ld \n", tm.tm_hour,tm.tm_min,tm.tm_sec);
+    printf("Nascimento do Filho1. Hora: %ld:%ld:%ld Data: %ld/%ld/%ld\n", tm.tm_hour,tm.tm_min,tm.tm_sec,tm.tm_mday,tm.tm_mon+1,tm.tm_year+1900);
 
     int p = fork();
+    
     if (p > 0){ //Pai
+        sleep(2);
+        printf("Nascimento do Filho2. Hora: %ld:%ld:%ld Data: %ld/%ld/%ld\n", tm.tm_hour,tm.tm_min,tm.tm_sec,tm.tm_mday,tm.tm_mon+1,tm.tm_year+1900);
         int pp = fork(); //Criar Filho2
 
         if(pp == 0){ //Filho2
+            sleep(14);
+            printf("Nascimento do Neto2. Hora: %ld:%ld:%ld Data: %ld/%ld/%ld\n", tm.tm_hour,tm.tm_min,tm.tm_sec,tm.tm_mday,tm.tm_mon+1,tm.tm_year+1900);
             int pf2 = fork(); //Criar Neto2
 
             if (pf2 == 0){ //Neto2
@@ -35,6 +40,8 @@ int main(){
     }
     
     if (p == 0){ //Filho
+        sleep(12);
+        printf("Nascimento do Neto1. Hora: %ld:%ld:%ld Data: %ld/%ld/%ld\n", tm.tm_hour,tm.tm_min,tm.tm_sec,tm.tm_mday,tm.tm_mon+1,tm.tm_year+1900);
         int pf = fork();
 
         if (pf == 0){ //Neto1
